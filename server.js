572882646f3015c7ebd980dd511ed5a1f9bdbad1
barath sam <barathsam555.p@gmail.com>
2023-10-13 19:15:05 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
+
 
 const connectDB = require("./config/db");
 const app = express();
+app.use(cors());
 //Connect Database
 connectDB();
 
